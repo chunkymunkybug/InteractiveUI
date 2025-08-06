029303285303b05bcb4de3ba3389fe1ba0d6811f
Maddy <m.s.woo2008@gmail.com>
2025-08-06 14:27:10 -0400

    deleted boilerplate + added text

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        VStack { //beginning of VStack
+            
+            Text ("What is your name?")
+                .font(.title)
+            
+        } //end of VStack
+        
+        
+        
         .padding()
     }
 }
