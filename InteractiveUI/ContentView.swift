b0f43f00a5752e4fb2ce1b2e0bd9704d398e7d0c
Maddy <m.s.woo2008@gmail.com>
2025-08-06 14:53:05 -0400

    Added actions to our elements (@State property wrapper + modifications )

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,20 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = "" //var that stores all names
+    //state tells SwiftUI to update the view whenever that variable is modified.
+    @State private var textTitle = "What is your name?"
     var body: some View {
+        
         VStack { //beginning of VStack
             
-            Text ("What is your name?")
+            Text (textTitle)
                 .font(.title)
                 .fontWeight(.bold) //customizes question
             
-            TextField("Type your name here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("Type your name here", text: $name) //binds var
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1) //custom text field
             
             Button("Submit Name") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                textTitle = "Welcome, \(name) !" //action of button
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
