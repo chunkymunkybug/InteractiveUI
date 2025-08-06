6410703aec33ee1e95472ed1b8fc475260f3f850
Maddy <m.s.woo2008@gmail.com>
2025-08-06 14:38:46 -0400

    Added button and modifiers

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -13,14 +13,30 @@ struct ContentView: View {
             
             Text ("What is your name?")
                 .font(.title)
+                .fontWeight(.bold) //customizes question
             
+            TextField("Type your name here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1) //custom text field
+            
+            Button("Submit Name") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple) //customizes button
+
+            
+            .padding () //padding around elements
         } //end of VStack
-        
-        
         
         .padding()
     }
 }
+
+
+
 
 #Preview {
     ContentView()
